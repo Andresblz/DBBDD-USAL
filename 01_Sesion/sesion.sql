3666bad53cde21f94d412b62b75ef11221a9443d
Andresblz <andresblz@usal.es>
2024-01-21 12:57:38 +0100

    :tada: Update Sesion 1

--- 01_Sesion/sesion.sql
+++ 01_Sesion/sesion.sql
@@ -4,11 +4,11 @@
  * 1. Obtener toda la información almacenada en la base de datos, relacionada con los 
  *    autores, sucursales, lectores, libros y editoriales. 
  */
-SELECT * FROM univ.autor;
-SELECT * FROM univ.lector;
-SELECT * FROM univ.sucursal;
-SELECT * FROM univ.libro;
-SELECT * FROM univ.editorial;
+SELECT * FROM autor;
+SELECT * FROM lector;
+SELECT * FROM sucursal;
+SELECT * FROM libro;
+SELECT * FROM editorial;
 
 /*
  * 2. Obtener la fecha de nacimiento de cada uno de los lectores ordenados del más 
@@ -20,19 +20,19 @@ SELECT fecha_nac FROM lector ORDER BY fecha_nac DESC;
  * 3. Obtener el ISBN de los libros que están prestados indicando la sucursal y la fecha en 
  *    la que se realizó el préstamo. Ordenar la salida por sucursal y fecha de préstamo.
  */
-SELECT ISBN, Cod_Suc, fecha_ini FROM prestamo WHERE fecha_dev IS NOT NULL ORDER BY Cod_Suc, fecha_ini ASC;
+SELECT isbn, cod_suc, fecha_ini FROM prestamo WHERE fecha_dev IS NOT NULL ORDER BY cod_suc, fecha_ini ASC;
 
 /*
  * 4. Obtener el código y nombre de las editoriales. Incluir entre ambos campos el literal 
  *    NOMBRE. 
  */
-SELECT codigo || 'NOMBRE' || Nombre FROM univ.editorial;
+SELECT codigo || ' NOMBRE ' || nombre FROM editorial;
 
 /*
  * 5. Obtener primer apellido, segundo apellido y nombre de todos los lectores de la 
  *    biblioteca ordenados primer y segundo apellido. 
  */
-SELECT ape_1 || '' || ape_2 || '' || nombre FROM lector ORDER BY ape_1, ape_2;
+SELECT ape_1, ape_2, nombre FROM lector ORDER BY ape_1, ape_2;
 
 /*
  * 6. Obtener un listado de los libros ordenado de menor a mayor antigüedad. 
@@ -46,6 +46,7 @@ SELECT * FROM libro ORDER BY ano_edicion DESC;
  *    se hubiera obtenido sin utilizar DISTINCT. 
  */
 SELECT DISTINCT poblacion, provincia FROM lector ORDER BY provincia, poblacion;
+
 SELECT poblacion, provincia FROM lector ORDER BY provincia, poblacion;
 
 /*
@@ -57,13 +58,14 @@ SELECT direccion FROM lector WHERE ape_2 = 'BENITO';
  * 9. Obtener el nombre completo de los autores con nacionalidad española (Busque
  *    previamente el código correspondiente en la tabla nacionalidad). 
  */
-SELECT codigo FROM univ.nacionalidad WHERE nombre = 'ESPANA';
-SELECT apellido || '' || nombre AS autor FROM univ.autor WHERE cod_nacion = '9';
+SELECT codigo FROM nacionalidad WHERE nombre = 'ESPANA';
+
+SELECT apellido || ' ' || nombre AS autor FROM autor WHERE cod_nacion = '9';
 
 /*
  * 10. Obtener la dirección postal completa de la sucursal cuyo código es 12 
  */
-SELECT direccion || ',' || poblacion || ',' || provincia AS direccion FROM univ.sucursal where codigo = '12';
+SELECT direccion || ',' || poblacion || ',' || provincia AS direccion FROM sucursal where codigo = '12';
 
 /*
  * 11. De los diferentes libros de los que disponen las sucursales indicar aquellas
@@ -71,19 +73,19 @@ SELECT direccion || ',' || poblacion || ',' || provincia AS direccion FROM univ.
  *     la sucursal, el ISBN del libro y el número de ejemplares con el requisito requerido
  *     (3 o más de 3 ejemplares). 
  */
-SELECT cod_suc, ISBN, num_ejemplares FROM univ.dispone WHERE num_ejemplares > 3 ORDER BY cod_suc ASC;
+SELECT cod_suc, isbn, num_ejemplares FROM dispone WHERE num_ejemplares > 3 ORDER BY cod_suc ASC;
 
 /*
  * 12. Obtener el código de los lectores s que han cogido en préstamos el libro de ISBN
  *     5023024 ordenados por sucursal y fecha de préstamo.
  */
-SELECT cod_lector FROM prestamo WHERE ISBN = '5023024' ORDER BY cod_suc, fecha_ini ASC;
+SELECT cod_lector FROM prestamo WHERE isbn = '5023024' ORDER BY cod_suc, fecha_ini ASC;
 
 /*
  * 13. Obtener el Código de los autores ya fallecidos, la fecha en la que fallecieron y los
  *     años que tenían. 
  */
-SELECT codigo, ano_fall, ano_fall-ano_nac FROM univ.autor WHERE ano_fall IS NOT NULL;
+SELECT codigo, ano_fall, ano_fall-ano_nac FROM autor WHERE ano_fall IS NOT NULL;
 
 /*
  * 14. Obtener nombre y dos apellidos de los lectores que hayan nacido antes de 1980. 
@@ -128,7 +130,7 @@ SELECT codigo, cod_lector, fecha_ini FROM prestamo WHERE fecha_dev IS NULL ORDER
  *     francesa o británica (Busque previamente los códigos correspondientes en la tabla
  *     nacionalidad). 
  */
-SELECT nombre, apellido FROM univ.autor WHERE cod_nacion = '9' OR cod_nacion = '11' OR cod_nacion = '25';
+SELECT nombre, apellido FROM autor WHERE cod_nacion = '9' OR cod_nacion = '11' OR cod_nacion = '25';
 
 /*
  * 22. Obtener un listado que incluya el código, nombre y provincia de los lectores que no
@@ -157,19 +159,18 @@ SELECT nombre, ape_1, ape_2 FROM lector WHERE length(ape_1) = 7;
  * 26. Seleccionar los lectores que tengan edades comprendidas entre los 26 y los 36 años.
  *     Ordenar de mayor a menor edad. 
  */
-SELECT nombre, ape_1, ape_2 FROM lector WHERE (to_char(sysdate, 'yyyy')-to_char(fecha_nac, 'yyyy')) BETWEEN 26 and 36
+SELECT nombre, ape_1, ape_2, fecha_nac FROM lector WHERE (to_char(sysdate, 'yyyy') - to_char(fecha_nac, 'yyyy') BETWEEN 26 and 36) ORDER BY fecha_nac DESC;
 
 /*
  * 27. Seleccionar los autores que no son españoles. 
  */
-SELECT nombre, apellido FROM univ.autor WHERE cod_nacion != '9';
+SELECT nombre, apellido FROM autor WHERE cod_nacion != '9';
 
 /*
  * 28. Mostrar los nombres, apellidos y edad de los autores vivos que no son españoles y
  *     que tienen más de 70 años o menos de 50. 
  */
-SELECT nombre, apellido, (to_char(sysdate, 'yyyy')-ano_nac) AS edad FROM univ.autor WHERE ano_fall IS NULL AND
-cod_nacion != '9' AND (to_char(sysdate, 'yyyy')-ano_nac) NOT BETWEEN 50 and 70;
+SELECT nombre, apellido, (to_char(sysdate, 'yyyy') - ano_nac) AS edad FROM autor WHERE ano_fall IS NULL AND cod_nacion != '9' AND (to_char(sysdate, 'yyyy') - ano_nac) NOT BETWEEN 50 and 70;
 
 /*
  * 29. Obtener los datos de los préstamos que, o bien son de la sucursal 5 y han sido
